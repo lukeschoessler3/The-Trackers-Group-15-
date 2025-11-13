cd82b829141baa25451e883d94fdb26dd075f0b2
Shane Thomson <shanet02@vt.edu>
2025-11-13 10:44:25 -0500

    Add delete_meal function

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ def add_meal(meal):
     calories = int(input("Enter calories: "))
     ingredients = input("Enter ingredients (comma separated): ").split(',')
     meal.append(Meal(name, calories, [ingredient.strip() for ingredient in ingredients]))
+
+def delete_meal(meals):
+    name = input("Which meal do you want to delete")
+    for m in meals:
+        if m.name == name:
+            meals.remove(m)
+            print(f"{name} has been deleted.")
+            return
+    print(f"{name} not found.")
