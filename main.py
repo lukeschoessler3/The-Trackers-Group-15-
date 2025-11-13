8409d68e88d1bc66418efbf582cdc1685c2d7f35
tberros <tberros@vt.edu>
2025-11-13 10:03:18 -0500

    meal class

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 # Main Program
+
+class Meal:
+    def __init__(self, name, calories, ingredients):
+        self.name = name
+        self.calories = calories
+        self.ingredients = ingredients
