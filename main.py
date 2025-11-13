af34a275cfdfd29871ac4287ab334cc42097601b
christopherq <christopherq@vt.edu>
2025-11-13 10:18:48 -0500

    code change

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ class Meal:
         self.name = name
         self.calories = calories
         self.ingredients = ingredients
+        
+def add_meal(meal):
+    name = input("Enter meal name: ")
+    calories = int(input("Enter calories: "))
+    ingredients = input("Enter ingredients (comma separated): ").split(',')
+    meal.append(Meal(name, calories, [ingredient.strip() for ingredient in ingredients]))
